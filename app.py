290854f9c10440d7a753eb1951885fe2d0b9200c
Rosy <rosypaul375@gmail.com>
2026-05-09 20:42:39 +0530

    code changes

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 # linting-check:flake8 app.py
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
 
-
-
-
-
-
-
-def sub(a,b):return a-b
-
-result1=add(4,5)
-result2=sub(6,2)
-print(result1,result2)
+result1 = add(4, 5)
+result2 = sub(6, 2)
+print(result1, result2)
